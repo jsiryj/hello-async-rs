f80fa0c097c6dcf58e2b164b89f2da1e5e3e329f
jsiryj <jsiryj@gmail.com>
2025-11-26 16:07:11 +0000

    Chain awaits

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -5,8 +5,7 @@ fn main() {
 }
 
 async fn page_title(url: &str) -> Option<String> {
-    let response = trpl::get(url).await;
-    let response_text = response.text().await;
+    let response_text = trpl::get(url).await.text().await;
     Html::parse(&response_text)
         .select_first("title")
         .map(|title_element| title_element.inner_html())
