a6febe93c5959fe3ae685c74b1f019988a65037f
jsiryj <jsiryj@gmail.com>
2025-11-26 16:24:57 +0000

    Work on main

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -4,10 +4,19 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     trpl::run(async {
-        let url = &args[1];
-        match page_title(url).await {
-            Some(title) => println!("The title for {url} was {title}"),
-            None => println!("{url} had no title"),
+        let title_fut_1 = page_title(&args[1]);
+        let title_fut_2 = page_title(&args[2]);
+
+        let (url, maybe_title) =
+            match trpl::race(title_fut_1, title_fut_2).await {
+                Either::Left(left) => left,
+                Either::Right(right) => right,
+            };
+        
+        println!("{url} returned first");
+        match maybe_title {
+            Some(title) => println!("Its page title is: '{title}'"),
+            None => println!("Its title could not be parsed."),
         }
     })
 }
